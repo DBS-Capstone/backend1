736f6eed35ef790897f863da7664c018f78099f4
Matt <114587056+MattYudha@users.noreply.github.com>
2025-06-10 19:46:35 +0700

    Update ConfigModule di AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'; 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BirdsModule } from '@birds/birds.module';
+import { BirdsModule } from './birds/birds.module'; 
 
 @Module({
-    imports: [
-        BirdsModule
-    ],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+    
+      isGlobal: true, 
+    }),
+    BirdsModule, 
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
